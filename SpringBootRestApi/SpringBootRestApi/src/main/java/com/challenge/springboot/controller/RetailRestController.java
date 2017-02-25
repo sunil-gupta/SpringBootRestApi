6e4d1ee89dc1db438e0037b763b7ec1c0f9364aa
sunil-gupta <sunilkumar.gupta06@gmail.com>
2017-02-25 17:32:27 +0530

    added method getShopNamesByLatitudeAndLongitude

--- SpringBootRestApi/SpringBootRestApi/src/main/java/com/challenge/springboot/controller/RetailRestController.java
+++ SpringBootRestApi/SpringBootRestApi/src/main/java/com/challenge/springboot/controller/RetailRestController.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.challenge.springboot.model.ShopDetails;
 import com.challenge.springboot.service.AddressService;
+import com.challenge.springboot.util.CustomErrorType;
 
 @RestController
 @RequestMapping("/api")
@@ -22,9 +24,7 @@ public class RetailRestController {
 	@Autowired
 	AddressService addressService; //Service which will do all data retrieval/manipulation work
 
-	
-
-	// -------------------Create a User-------------------------------------------
+	// -------------------Create a Shop Address-------------------------------------------
 
 	@RequestMapping(value = "/shop/", method = RequestMethod.POST)
 	public ResponseEntity<?> createShopAddress(@RequestBody ShopDetails shopReq) {
@@ -35,12 +35,42 @@ public class RetailRestController {
 			return new ResponseEntity(new CustomErrorType("Unable to create. A User with name " + 
 			user.getName() + " already exist."),HttpStatus.CONFLICT);
 		}*/
-		addressService.saveShopAddress(shopReq);
+		
+		try {
+			addressService.saveShopAddress(shopReq);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		
 		return new ResponseEntity<ShopDetails>(shopReq, HttpStatus.CREATED);
 	}
-
 	
+	
+	/**
+	 * @param latitude
+	 * @param longitude
+	 * @return
+	 */
+	@RequestMapping(value = "/shopNames/", method = RequestMethod.GET)
+	public ResponseEntity<?> getShopNamesByLatitudeAndLongitude(@RequestParam("latitude") String latitude,@RequestParam("longitude") String longitude) {
 
+		logger.info("Fetching Shops Name with latitude and longitude", latitude +" "+longitude);
+		String[] shopNames = null;
+		try {
+			shopNames = addressService.findById(latitude, longitude);
+			if (shopNames == null || shopNames.length==0) {
+				logger.error("Shops with latitude and longitude not found.", latitude +" "+longitude);
+				
+				return new ResponseEntity<CustomErrorType>(new CustomErrorType("Shops with latitude and longitude" + latitude +" "+longitude 
+						+ " not found"), HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<String[]>(shopNames, HttpStatus.OK);
+	}
 }
